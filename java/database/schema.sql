d4ecbb54d3d78911a27089891910d4f8b1d5d87a
meglemon <meganmadalynn@gmail.com>
2024-01-11 17:53:33 -0500

    DB schemas script complete

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,20 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE lists (
+    list_id SERIAL,
+    list_name varchar(100) NOT NULL,
+    user_id int NOT NULL,
+    CONSTRAINT PK_list_id PRIMARY KEY (list_id),
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id)
+);
+
+CREATE TABLE items (
+  item_id SERIAL,
+  item_name varchar (100) NOT NULL,
+  list_id int NOT NULL,
+  CONSTRAINT PK_item_id PRIMARY KEY (item_id),
+  CONSTRAINT FK_list_id FOREIGN KEY (list_id)
+);
+
 COMMIT TRANSACTION;
