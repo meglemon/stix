498fbc7254777bcaefab85614978e7c33575a764
meglemon <meganmadalynn@gmail.com>
2024-02-07 10:58:07 -0500

    connections are completed across all ends

--- java/database/create.sh
+++ java/database/create.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/zsh
 export PGPASSWORD='postgres1'
 BASEDIR=$(dirname $0)
-DATABASE=stix
+DATABASE=final_capstone
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
--- java/database/dropdb.sql
+++ java/database/dropdb.sql
@@ -5,9 +5,9 @@
 -- The following line terminates any active connections to the database so that it can be destroyed
 SELECT pg_terminate_backend(pid)
 FROM pg_stat_activity
-WHERE datname = 'stix';
+WHERE datname = 'final_capstone';
 
-DROP DATABASE stix;
+DROP DATABASE final_capstone;
 
 DROP USER final_capstone_owner;
 DROP USER final_capstone_appuser;
