a55572eeeda629ebc33b9cbdea6f6b45682c63c8
meglemon <meganmadalynn@gmail.com>
2024-02-12 14:57:47 -0500

    able to show user lists

--- vue/src/components/RandomSelector.vue
+++ vue/src/components/RandomSelector.vue
@@ -2,13 +2,27 @@
     <div>
         <label for="list-selector">Select a Stix Collection</label>
         <select name="list-selector" id="list-selector">
-            <option>list items</option>
+            <option v-for="listName in lists">{{ listName }}</option>
         </select>
     </div>
 </template>
 
 <script>
+import ListsService from '../services/ListsService';
 export default {
 
+    data(){
+        return{
+            listName: '',
+            lists: []
+        }
+
+    },
+
+    created(){
+        ListsService
+        .getListsByUserId(this.$store.state.user.id)
+        .then((response)=> {this.lists = response.data})
+    }
 }
 </script>
